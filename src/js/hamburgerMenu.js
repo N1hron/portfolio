11b38f6008e9bd06128b352536fa16b772371b01
N1hron <denik8008@gmail.com>
2023-03-29 01:15:14 +0300

    update 4

--- src/js/hamburgerMenu.js
+++ src/js/hamburgerMenu.js
@@ -8,14 +8,16 @@ export default function hamburgerMenu(triggerSelector, menuSelector, closeSelect
         menu.classList.add('sidepanel_active');
         overlay.style.transition = 'opacity 0.5s, visibility 0.5s';
         overlay.classList.add('overlay_active');
-        document.body.style.marginRight = (window.innerWidth - document.body.scrollWidth) + 'px';
+        // document.body.style.marginRight = (window.innerWidth - document.body.scrollWidth) + 'px';
+        document.documentElement.style.overflow = 'hidden';
         document.body.style.overflow = 'hidden';
     })
 
     close.addEventListener('click', (event) => {
         menu.classList.remove('sidepanel_active');
         overlay.classList.remove('overlay_active');
-        document.body.style.marginRight = 0;
-        document.body.style.overflow = 'auto';
+        // document.body.style.marginRight = 0;
+        document.documentElement.style.overflow = 'auto';
+        document.body.style.overflow = 'hidden';
     })
 }
