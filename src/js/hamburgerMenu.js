55ac173b29521545d9c93118d80c6c3b590cec36
N1hron <denik8008@gmail.com>
2023-03-30 01:46:57 +0300

    update 5

--- src/js/hamburgerMenu.js
+++ src/js/hamburgerMenu.js
@@ -3,21 +3,26 @@ export default function hamburgerMenu(triggerSelector, menuSelector, closeSelect
     const menu = document.querySelector(menuSelector);
     const close = document.querySelector(closeSelector);
     const overlay = document.querySelector('.overlay');
+    const buttons = menu.querySelectorAll("li a");
+
+    function onClose() {
+        menu.classList.remove('sidepanel_active');
+        overlay.classList.remove('overlay_active');
+        document.documentElement.style.overflow = 'auto';
+        document.body.style.overflow = 'hidden';
+    }
     
     trigger.addEventListener('click', (event) => {
         menu.classList.add('sidepanel_active');
         overlay.style.transition = 'opacity 0.5s, visibility 0.5s';
         overlay.classList.add('overlay_active');
-        // document.body.style.marginRight = (window.innerWidth - document.body.scrollWidth) + 'px';
         document.documentElement.style.overflow = 'hidden';
         document.body.style.overflow = 'hidden';
     })
 
-    close.addEventListener('click', (event) => {
-        menu.classList.remove('sidepanel_active');
-        overlay.classList.remove('overlay_active');
-        // document.body.style.marginRight = 0;
-        document.documentElement.style.overflow = 'auto';
-        document.body.style.overflow = 'hidden';
+    close.addEventListener('click', () => onClose());
+
+    buttons.forEach(btn => {
+        btn.addEventListener('click', () => onClose());
     })
 }
